47a333988555e2488a444e302725a86db8a9413a
Felipe Immich <130936079+FelipeImmich@users.noreply.github.com>
2025-01-30 00:38:51 +0000

    Exibição formatada da carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,16 +5,17 @@
 
 int main() {
     // Definindo variáveis separadas para cada atributo da cidade.
-    char codigo_cidade[3], nome[50];
-    int populacao,numero_pontos_turisticos;
-    float area, PIB;
+    char codigo_cidade[4]="";
+    char nome[50]="";
+    int populacao,numero_pontos_turisticos=0;
+    float area, PIB=0;
     
     // Cadastro das Cartas: Entrada de dados
-    printf("Insira o código da cidade: ex.: A01");
-    scanf("%s", &codigo_cidade);
+    printf("Insira o código da cidade: ");
+    scanf("%s", codigo_cidade);
 
     printf("Insira o nome da cidade: ");
-    scanf("%s", &nome);
+    scanf("%s", nome);
 
     printf("Insira a população: ");
     scanf("%d", &populacao);
@@ -30,7 +31,13 @@ int main() {
     
     // Exibição dos Dados das Cartas: Saida Formatada
     
-
+    printf("A Seguinte carta foi cadastrada com sucesso.\n");
+    printf("Código da Cidade: %.3s\n", codigo_cidade);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População da cidade: %d habitantes\n", populacao);
+    printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos);
+    printf("Área: %.2f m²\n", area);
+    printf("PIB: %.2f\n", PIB);
 
     return 0;
 }
